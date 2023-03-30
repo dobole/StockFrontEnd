03b969102a223a37a48cd00c522b2b834a20cd00
chen <cyyyyjjj@163.com>
2023-03-30 10:43:29 +0800

    初始化项目

---
+++ src/store/modules/sidebar/state.js
@@ -0,0 +1,41 @@
+export default {
+  sidebarMap: {
+    1: {
+      main: [
+        {
+          groupTitle: '分组1'
+        },
+        {
+          path: '/main/first',
+          icon: 'suncaper-menu-unfold',
+          title: '一级菜单1'
+        },
+        {
+          path: '/main/second',
+          icon: 'suncaper-menu-unfold',
+          title: '一级菜单2',
+          group: 'second',
+          children: [
+            {
+              path: '/main/second/second',
+              icon: 'suncaper-menu-unfold',
+              title: '二级菜单'
+            }
+          ]
+        }
+      ]
+    }
+  },
+  sidebarTheme: {
+    dark: {
+      background: '#1f2c35',
+      text: '#ffffff',
+      activeText: '#ffffff'
+    },
+    light: {
+      background: '#ffffff',
+      text: '#000000',
+      activeText: '#1890ff'
+    }
+  }
+}
